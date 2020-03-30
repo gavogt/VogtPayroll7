1c379951acd9de10382e4df5c4a994ef038ebbf5
Gabriel <gvogt@asu.edu>
2020-03-29 17:30:56 -0700

    Put the eastern bonus amount calculation in the constructor

--- VogtPayroll7/Employee.cs
+++ VogtPayroll7/Employee.cs
@@ -9,16 +9,25 @@ namespace VogtPayroll7
         private string _empName;
 
         public int YearsOfService { get; set; }
-        public decimal BonusAmount { get; set; }
+        public decimal EasterBonusAmount { get; set; }
 
         public Employee(string empName, int yearsOfService)
         {
             _empName = empName;
             YearsOfService = yearsOfService;
 
+            if (YearsOfService >= 1 && YearsOfService <= 4)
+            {
+                EasterBonusAmount = 100m;
+            }
+            else if (YearsOfService >= 4 && YearsOfService <= 9)
+            {
+                EasterBonusAmount = 500m;
+            }
+            else if (YearsOfService >= 10)
+            {
+                EasterBonusAmount = 1000m;
+            }
         }
-
-
-
     }
 }
--- VogtPayroll7/Payroll.cs
+++ VogtPayroll7/Payroll.cs
@@ -19,38 +19,16 @@ namespace VogtPayroll7
             return _employees.Count;
         }
 
-        public void GetBonusAmount()
-        {
-            foreach (var employee in _employees)
-            {
-                if (employee.YearsOfService >= 1 && employee.YearsOfService <= 4)
-                {
-                    employee.BonusAmount = 100m;
-                }
-                else if (employee.YearsOfService >= 4 && employee.YearsOfService <= 9)
-                {
-                    employee.BonusAmount = 500m;
-                }
-                else if(employee.YearsOfService >= 10)
-                {
-                    employee.BonusAmount = 1000m;
-                }
-                else
-                {
-                    Console.WriteLine("Years need to be greater than equal to one!");
-                }
-            }
-        }
+
 
         public decimal GetTotalAmountAwardedForAllEmployees()
         {
             decimal totalAmount = 0m;
-
-            GetBonusAmount();
+            Employee emp = default;
 
             foreach (var employee in _employees)
             {
-                totalAmount += employee.BonusAmount;
+                totalAmount += employee.EasterBonusAmount;
             }
 
             Console.WriteLine($"Total amount awarded: {totalAmount:C2}");
